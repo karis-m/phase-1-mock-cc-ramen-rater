d4baab9fb16b55884551e5d423f7d50425b88f0f
moses <moses.kamau@student.moringaschool.com>
2023-07-07 23:50:15 +0300

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 // write your code here
+async function ramenMenu(){
+    const baseUrl = 'http://localhost:3000/ramens'
+    const response = await fetch(baseUrl, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }})
+        .then(response => (console.log(response)))
+       // .then(ramens => (console.log(ramens)));
+
+}
